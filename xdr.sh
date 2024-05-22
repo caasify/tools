8f37305d532adb641f0228b78306ffd3b84e783b
Alireza Pourmahdavi <arzpmv@gmail.com>
2024-05-22 12:39:37 +0300

    Update xdr.sh

--- xdr.sh
+++ xdr.sh
@@ -68,10 +68,10 @@ sed -i.bak 's/HOME_NET: "\[192.168.0.0\/16,10.0.0.0\/8,172.16.0.0\/12\]"/HOME_NE
 
 
 suricata-update
-sleep 20
 
 systemctl daemon-reload
 systemctl enable suricata 
 systemctl enable wazuh-agent
 systemctl start suricata 
 systemctl start wazuh-agent
+systemctl restart suricata
